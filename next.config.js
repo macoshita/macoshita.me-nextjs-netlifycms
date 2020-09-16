e54679818d86a791d3544176e0a3c2618c812f94
macoshita <macoshita@gmail.com>
2020-09-16 22:39:20 +0900

    Add emoji & fix breaks

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const withMdxEnhanced = require("next-mdx-enhanced");
 const rehypePrism = require("@mapbox/rehype-prism");
+const emoji = require('remark-emoji');
+const breaks = require('remark-breaks');
 
 module.exports = withMdxEnhanced({
   layoutPath: "src/layouts",
   defaultLayout: true,
+  remarkPlugins: [breaks, emoji],
   rehypePlugins: [rehypePrism],
 })({
   pageExtensions: ["mdx", "tsx"],
